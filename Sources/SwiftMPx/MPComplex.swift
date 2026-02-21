f95d794a55c3f4867986c9278f2abc44619da859
zapccu <d.braner@gmx.net>
2026-02-21 17:12:06 +0100

    Fixed runtime errors. Optimized memory allocation.

--- Sources/SwiftMPx/MPComplex.swift
+++ Sources/SwiftMPx/MPComplex.swift
@@ -5,115 +5,366 @@
 //  Created by Dirk Braner on 14.02.26.
 //
 
-#if canImport(Numerics)
-import Numerics
-#endif
-
 import CMPFR
 
-public class MPComplex {
-    var real: mpfr_t
-    var imaginary: mpfr_t
+public class MPComplex : CustomStringConvertible {
+    public var real: MPFloat
+    public var imaginary: MPFloat
     
     // Precision (default is 128 bit)
     public let precision: Int32
 
+    // Temporary variables, prevent memory allocation
+    public var tmp1: MPFloat
+    public var tmp2: MPFloat
+    public var tmp3: MPFloat
+    public var tmp4: MPFloat
+    
+    /// Initialize MPComplex
     init(prec: Int32 = 128) {
-        precision = prec
-        real = mpfr_t()
-        mpfr_init2(&real, mpfr_prec_t(prec))
-        imaginary = mpfr_t()
-        mpfr_init2(&imaginary, mpfr_prec_t(prec))
+        self.precision = prec
+        self.real = MPFloat(prec: prec)
+        self.imaginary = MPFloat(prec: prec)
+        self.tmp1 = MPFloat(prec: prec)
+        self.tmp2 = MPFloat(prec: prec)
+        self.tmp3 = MPFloat(prec: prec)
+        self.tmp4 = MPFloat(prec: prec)
     }
     
+    /// Initialize MPComplex with Double values
     public init(_ real: Double, _ imaginary: Double = 0.0, precision: Int32 = 128) {
         self.precision = precision
-
-        self.real = mpfr_t()
-        mpfr_init2(&self.real, mpfr_prec_t(precision))
-        mpfr_set_d(&self.real, real, MPFR_RNDN)
-
-        self.imaginary = mpfr_t()
-        mpfr_init2(&self.imaginary, mpfr_prec_t(precision))
-        mpfr_set_d(&self.imaginary, imaginary, MPFR_RNDN)
+        self.real = MPFloat(real, precision: precision)
+        self.imaginary = MPFloat(imaginary, precision: precision)
+        self.tmp1 = MPFloat(prec: precision)
+        self.tmp2 = MPFloat(prec: precision)
+        self.tmp3 = MPFloat(prec: precision)
+        self.tmp4 = MPFloat(prec: precision)
     }
     
-    public init(_ real: String = "0", _ imaginary: String = "0", precision: Int32 = 128) {
+    /// Initialize MPComplex with String values
+    public init(_ real: String, _ imaginary: String = "0", precision: Int32 = 128) {
         self.precision = precision
-
-        self.real = mpfr_t()
-        mpfr_init2(&self.real, mpfr_prec_t(precision))
-        mpfr_set_str(&self.real, real, 10, MPFR_RNDN)
-        
-        self.imaginary = mpfr_t()
-        mpfr_init2(&self.imaginary, mpfr_prec_t(precision))
-        mpfr_set_str(&self.imaginary, real, 10, MPFR_RNDN)
+        self.real = MPFloat(real, precision: precision)
+        self.imaginary = MPFloat(imaginary, precision: precision)
+        self.tmp1 = MPFloat(prec: precision)
+        self.tmp2 = MPFloat(prec: precision)
+        self.tmp3 = MPFloat(prec: precision)
+        self.tmp4 = MPFloat(prec: precision)
     }
     
-    /// Free memory
-    deinit {
-        mpfr_clear(&self.real)
-        mpfr_clear(&self.imaginary)
+    /// Initialize MPComplex with MPFloat values
+    public init(_ real: MPFloat, _ imaginary: MPFloat = MPFloat(0.0), precision: Int32 = 128) {
+        self.precision = precision
+        self.real = MPFloat(prec: precision)
+        self.imaginary = MPFloat(prec: precision)
+        self.real.set(real)
+        self.imaginary.set(imaginary)
+        self.tmp1 = MPFloat(prec: precision)
+        self.tmp2 = MPFloat(prec: precision)
+        self.tmp3 = MPFloat(prec: precision)
+        self.tmp4 = MPFloat(prec: precision)
+    }
+    
+    /// Make MPComplex printable
+    public var description: String {
+        return "\(self.real.toString()) + \(self.imaginary.toString())i"
+    }
+    
+    public var length: MPFloat {
+        self.abs()
+    }
+    
+    public var lengthSquared: MPFloat {
+        self.norm()
     }
     
     /// Return a copy of a value
     /// Use y = x.copy() instead of y = x
     public func copy() -> MPComplex {
         let newObj = MPComplex(prec: self.precision)
-        mpfr_set(&newObj.real, &self.real, MPFR_RNDN)
-        mpfr_set(&newObj.imaginary, &self.imaginary, MPFR_RNDN)
+        newObj.real.set(self.real)
+        newObj.imaginary.set(self.imaginary)
         return newObj
     }
     
     /// Set value to String
     public func set(_ real: String, _ imaginary: String = "0") {
-        mpfr_set_str(&self.real, real, 10, MPFR_RNDN)
-        mpfr_set_str(&self.imaginary, imaginary, 10, MPFR_RNDN)
+        self.real.set(real)
+        self.imaginary.set(imaginary)
     }
     
     /// Set value to Double
     public func set(_ real: Double, _ imaginary: Double = 0.0) {
-        mpfr_set_d(&self.real, real, MPFR_RNDN)
-        mpfr_set_d(&self.imaginary, imaginary, MPFR_RNDN)
+        self.real.set(real)
+        self.imaginary.set(imaginary)
     }
     
     /// Set value to value of MPFloat (deep copy)
     public func set(_ real: MPFloat, _ imaginary: MPFloat) {
-        mpfr_set(&self.real, &real.value, MPFR_RNDN)
-        mpfr_set(&self.imaginary, &imaginary.value, MPFR_RNDN)
+        self.real.set(real)
+        self.imaginary.set(imaginary)
     }
     
     /// Set value to value of MPComplex (deep copy)
     public func set(_ cval: MPComplex) {
-        mpfr_set(&self.real, &cval.real, MPFR_RNDN)
-        mpfr_set(&self.imaginary, &cval.imaginary, MPFR_RNDN)
+        self.real.set(cval.real)
+        self.imaginary.set(cval.imaginary)
     }
-    
-    #if canImport(Numerics)
-
-    /// Set value to Complex<Double>
-    public func set(_ cval: Complex<Double>) {
-        mpfr_set_d(&self.real, cval.real, MPFR_RNDN)
-        mpfr_set_d(&self.imaginary, cval.imaginary, MPFR_RNDN)
-    }
-    
-    /// Convert value to Complex<Double>
-    func toComplex() -> Complex<Double> {
-        return Complex<Double>(mpfr_get_d(&self.real, MPFR_RNDN), mpfr_get_d(&self.imaginary, MPFR_RNDN))
-    }
-    
-    #endif
     
     //
     // Addition
     //
+
+    /// Addition, return new value
+    public static func + (_ lhs: MPComplex, _ rhs: MPComplex) -> MPComplex {
+        let result = MPComplex(prec: lhs.precision)
+        mpfr_add(&result.real.value, &lhs.real.value, &rhs.real.value, MPFR_RNDN)
+        mpfr_add(&result.imaginary.value, &lhs.imaginary.value, &rhs.imaginary.value, MPFR_RNDN)
+        return result
+    }
+    
+    /// Addition, in-place
+    public static func += (lhs: inout MPComplex, rhs: MPComplex) {
+        mpfr_add(&lhs.tmp1.value, &lhs.real.value, &rhs.real.value, MPFR_RNDN)
+        mpfr_set(&lhs.real.value, &lhs.tmp1.value, MPFR_RNDN)
+        mpfr_add(&lhs.tmp1.value, &lhs.imaginary.value, &rhs.imaginary.value, MPFR_RNDN)
+        mpfr_set(&lhs.imaginary.value, &lhs.tmp1.value, MPFR_RNDN)
+    }
+    
+    //
+    // Subtraction
+    //
+    
+    /// Subtraction, return new value
+    public static func - (_ lhs: MPComplex, _ rhs: MPComplex) -> MPComplex {
+        let result = MPComplex(prec: lhs.precision)
+        mpfr_sub(&result.real.value, &lhs.real.value, &rhs.real.value, MPFR_RNDN)
+        mpfr_sub(&result.imaginary.value, &lhs.imaginary.value, &rhs.imaginary.value, MPFR_RNDN)
+        return result
+    }
+    
+    /// Subtraction, in-place
+    public static func -= (lhs: inout MPComplex, rhs: MPComplex) {
+        let tmp1 = MPFloat(prec: lhs.precision)
+        mpfr_sub(&tmp1.value, &lhs.real.value, &rhs.real.value, MPFR_RNDN)
+        mpfr_set(&lhs.real.value, &tmp1.value, MPFR_RNDN)
+        mpfr_sub(&tmp1.value, &lhs.imaginary.value, &rhs.imaginary.value, MPFR_RNDN)
+        mpfr_set(&lhs.imaginary.value, &tmp1.value, MPFR_RNDN)
+    }
+    
+    //
+    // Multiplication
+    //
+
+    /// Multiplication (MPComplex, MPComplex), return new value
+    public static func * (_ lhs: MPComplex, _ rhs: MPComplex) -> MPComplex {
+        let result = MPComplex(prec: lhs.precision)
+        
+        // result.real = lhs.real * rhs.real - lhs.imaginary * rhs.imaginary
+        mpfr_mul(&result.tmp1.value, &lhs.real.value, &rhs.real.value, MPFR_RNDN)
+        mpfr_mul(&result.imaginary.value, &lhs.imaginary.value, &rhs.imaginary.value, MPFR_RNDN)
+        mpfr_sub(&result.real.value, &result.tmp1.value, &result.imaginary.value, MPFR_RNDN)
+
+        // result.imaginary = lhs.real * rhs.imaginary + lhs.imaginary * rhs.real
+        mpfr_mul(&result.tmp1.value, &lhs.real.value, &rhs.imaginary.value, MPFR_RNDN)
+        mpfr_mul(&result.tmp2.value, &lhs.imaginary.value, &rhs.real.value, MPFR_RNDN)
+        mpfr_add(&result.imaginary.value, &result.tmp1.value, &result.tmp2.value, MPFR_RNDN)
+        
+        return result
+    }
+    
+    /// Multiplication (MPComplex, MPFloat), return new value
+    public static func * (_ lhs: MPComplex, _ rhs: MPFloat) -> MPComplex {
+        let result = MPComplex(prec: lhs.precision)
+
+        // result.real = lhs.real * rhs
+        mpfr_mul(&result.real.value, &lhs.real.value, &rhs.value, MPFR_RNDN)
+        
+        // result.imaginary = lhs.imaginary * rhs
+        mpfr_mul(&result.imaginary.value, &lhs.imaginary.value, &rhs.value, MPFR_RNDN)
+        
+        return result
+    }
+    
+    /// Multiplication (MPFloat, MPComplex), return new value
+    public static func * (_ lhs: MPFloat, _ rhs: MPComplex) -> MPComplex {
+        let result = MPComplex(prec: rhs.precision)
+
+        // result.real = lhs * rhs.real
+        mpfr_mul(&result.real.value, &lhs.value, &rhs.real.value, MPFR_RNDN)
+        
+        // result.imaginary = lhs * rhs.imaginary
+        mpfr_mul(&result.imaginary.value, &lhs.value, &rhs.imaginary.value, MPFR_RNDN)
+        
+        return result
+    }
+    
+    /// Multiplication (in place)
+    public static func *= (lhs: inout MPComplex, rhs: MPComplex) {
+        // result.real = lhs.real * rhs.real - lhs.imaginary * rhs.imaginary
+        mpfr_mul(&lhs.tmp1.value, &lhs.real.value, &rhs.real.value, MPFR_RNDN)
+        mpfr_mul(&lhs.tmp2.value, &lhs.imaginary.value, &rhs.imaginary.value, MPFR_RNDN)
+        mpfr_sub(&lhs.tmp3.value, &lhs.tmp1.value, &lhs.tmp2.value, MPFR_RNDN)
+
+        // result.imaginary = lhs.real * rhs.imaginary + lhs.imaginary * rhs.real
+        mpfr_mul(&lhs.tmp1.value, &lhs.real.value, &rhs.imaginary.value, MPFR_RNDN)
+        mpfr_mul(&lhs.tmp2.value, &lhs.imaginary.value, &rhs.real.value, MPFR_RNDN)
+        mpfr_add(&lhs.imaginary.value, &lhs.tmp1.value, &lhs.tmp2.value, MPFR_RNDN)
+        
+        mpfr_set(&lhs.real.value, &lhs.tmp3.value, MPFR_RNDN)
+    }
+    
+    /// Multiplication (in place)
+    public static func *= (lhs: inout MPComplex, rhs: MPFloat) {
+        // lhs.real = lhs.real * rhs
+        mpfr_mul(&lhs.real.value, &lhs.real.value, &rhs.value, MPFR_RNDN)
+        
+        // lhs.imaginary = lhs.imaginary * rhs
+        mpfr_mul(&lhs.imaginary.value, &lhs.imaginary.value, &rhs.value, MPFR_RNDN)
+    }
+
+    //
+    // Division
+    //
+    
+    /// Division: MPComplex, MPComplex
+    public static func / (_ lhs: MPComplex, _ rhs: MPComplex) -> MPComplex {
+        let result = MPComplex(prec: lhs.precision)
+
+        // tmp3 = rhs.real * rhs.real + rhs.imag * rhs.imag
+        mpfr_sqr(&result.tmp1.value, &rhs.real.value, MPFR_RNDN)
+        mpfr_sqr(&result.tmp2.value, &rhs.imaginary.value, MPFR_RNDN)
+        mpfr_add(&result.tmp3.value, &result.tmp1.value, &result.tmp2.value, MPFR_RNDN)
+        
+        // result.real = (lhs.real * rhs.real + lhs.imag * rhs.imag) / tmp3
+        mpfr_mul(&result.real.value, &lhs.real.value, &rhs.real.value, MPFR_RNDN)
+        mpfr_mul(&result.tmp1.value, &lhs.imaginary.value, &rhs.imaginary.value, MPFR_RNDN)
+        mpfr_add(&result.tmp2.value, &result.real.value, &result.tmp1.value, MPFR_RNDN)
+        mpfr_div(&result.real.value, &result.tmp2.value, &result.tmp3.value, MPFR_RNDN)
+        
+        // result.imag = (rhs.real * lhs.imag - lhs.real * rhs.imag) / tmp3
+        mpfr_mul(&result.imaginary.value, &rhs.real.value, &lhs.imaginary.value, MPFR_RNDN)
+        mpfr_mul(&result.tmp1.value, &lhs.real.value, &rhs.imaginary.value, MPFR_RNDN)
+        mpfr_sub(&result.tmp2.value, &result.imaginary.value, &result.tmp1.value, MPFR_RNDN)
+        mpfr_div(&result.imaginary.value, &result.tmp2.value, &result.tmp3.value, MPFR_RNDN)
+        
+        return result
+    }
+    
+    /// Division: MPComplex, MPFloat
+    public static func / (_ lhs: MPComplex, _ rhs: MPFloat) -> MPComplex {
+        let result = MPComplex(prec: lhs.precision)
+
+        mpfr_ui_div(&result.tmp1.value, 1, &rhs.value, MPFR_RNDN)
+
+        // result.real = lhs.real * tmp1
+        mpfr_mul(&result.real.value, &lhs.real.value, &result.tmp1.value, MPFR_RNDN)
+        
+        // result.imaginary = lhs.imaginary * tmp1
+        mpfr_mul(&result.imaginary.value, &lhs.imaginary.value, &rhs.value, MPFR_RNDN)
+        
+        return result
+    }
+    
+    /// Division: MPFloat, MPComplex
+    public static func / (_ lhs: MPFloat, _ rhs: MPComplex) -> MPComplex {
+        let result = MPComplex(prec: rhs.precision)
+
+        mpfr_ui_div(&result.tmp1.value, 1, &lhs.value, MPFR_RNDN)
+
+        // result.real = rhs.real * tmp1
+        mpfr_mul(&result.real.value, &rhs.real.value, &result.tmp1.value, MPFR_RNDN)
+        
+        // result.imaginary = rhs.imaginary * tmp1
+        mpfr_mul(&result.imaginary.value, &rhs.imaginary.value, &result.tmp1.value, MPFR_RNDN)
+        
+        return result
+    }
+    
+    /// Division, in-place
+    public static func /= (lhs: inout MPComplex, rhs: MPComplex) {
+        let result = MPComplex(prec: lhs.precision)
+
+        // tmp3 = rhs.real * rhs.real + rhs.imag * rhs.imag
+        mpfr_sqr(&lhs.tmp1.value, &rhs.real.value, MPFR_RNDN)
+        mpfr_sqr(&lhs.tmp2.value, &rhs.imaginary.value, MPFR_RNDN)
+        mpfr_add(&lhs.tmp3.value, &lhs.tmp1.value, &lhs.tmp2.value, MPFR_RNDN)
+        
+        // result.real = (lhs.real * rhs.real + lhs.imag * rhs.imag) / tmp3
+        mpfr_mul(&result.real.value, &lhs.real.value, &rhs.real.value, MPFR_RNDN)
+        mpfr_mul(&lhs.tmp1.value, &lhs.imaginary.value, &rhs.imaginary.value, MPFR_RNDN)
+        mpfr_add(&lhs.tmp2.value, &result.real.value, &lhs.tmp1.value, MPFR_RNDN)
+        mpfr_div(&result.real.value, &lhs.tmp2.value, &lhs.tmp3.value, MPFR_RNDN)
+        
+        // result.imag = (rhs.real * lhs.imag - lhs.real * rhs.imag) / tmp3
+        mpfr_mul(&result.imaginary.value, &rhs.real.value, &lhs.imaginary.value, MPFR_RNDN)
+        mpfr_mul(&lhs.tmp1.value, &lhs.real.value, &rhs.imaginary.value, MPFR_RNDN)
+        mpfr_sub(&lhs.tmp2.value, &result.imaginary.value, &lhs.tmp1.value, MPFR_RNDN)
+        mpfr_div(&lhs.imaginary.value, &lhs.tmp2.value, &lhs.tmp3.value, MPFR_RNDN)
+        
+        mpfr_set(&lhs.real.value, &result.real.value, MPFR_RNDN)
+    }
+   
+    /// Division (in place)
+    public static func /= (lhs: inout MPComplex, rhs: MPFloat) {
+        mpfr_ui_div(&lhs.tmp1.value, 1, &rhs.value, MPFR_RNDN)
+
+        // result.real = lhs.real * tmp1
+        mpfr_mul(&lhs.real.value, &lhs.real.value, &lhs.tmp1.value, MPFR_RNDN)
+        
+        // result.imaginary = lhs.imaginary * tmp1
+        mpfr_mul(&lhs.imaginary.value, &lhs.imaginary.value, &lhs.tmp1.value, MPFR_RNDN)
+    }
     
     //
     // Comparision
     //
     
     public static func == (lhs: MPComplex, rhs: MPComplex) -> Bool {
-        return mpfr_cmp(&lhs.real, &rhs.real) == 0 && mpfr_cmp(&lhs.imaginary, &rhs.imaginary) == 0
+        return lhs.real == rhs.real && lhs.imaginary == rhs.imaginary
     }
 
+    //
+    // Mathematic functions
+    //
+    
+    /// Square
+    public func square() -> MPComplex {
+        let result = MPComplex(prec: precision)
+
+        mpfr_sqr(&tmp1.value, &self.real.value, MPFR_RNDN)
+        mpfr_sqr(&tmp2.value, &self.imaginary.value, MPFR_RNDN)
+        mpfr_sub(&result.real.value, &tmp1.value, &tmp2.value, MPFR_RNDN)
+        
+        mpfr_mul_ui(&tmp1.value, &self.real.value, 2, MPFR_RNDN)
+        mpfr_mul(&result.imaginary.value, &tmp1.value, &self.imaginary.value, MPFR_RNDN)
+        
+        return result
+    }
+    
+    /// Norm / magnitude
+    public func norm () -> MPFloat {
+        let result = MPFloat(prec: precision)
+
+        mpfr_sqr(&tmp1.value, &self.real.value, MPFR_RNDN)
+        mpfr_sqr(&tmp2.value, &self.imaginary.value, MPFR_RNDN)
+        mpfr_add(&result.value, &tmp1.value, &tmp2.value, MPFR_RNDN)
+        
+        return result
+    }
+    
+    /// Absolute value
+    public func abs () -> MPFloat {
+        let result = MPFloat(prec: precision)
+
+        mpfr_sqr(&tmp1.value, &self.real.value, MPFR_RNDN)
+        mpfr_sqr(&tmp2.value, &self.imaginary.value, MPFR_RNDN)
+        mpfr_add(&tmp3.value, &tmp1.value, &tmp2.value, MPFR_RNDN)
+        mpfr_sqrt(&result.value, &tmp3.value, MPFR_RNDN)
+        
+        return result
+    }
+    
 }
+
--- Sources/SwiftMPx/MPFloat.swift
+++ Sources/SwiftMPx/MPFloat.swift
@@ -11,11 +11,16 @@ import CMPFR
 // Floating point type with variable precision
 //
 
-public class MPFloat : Comparable {
+infix operator <- : AssignmentPrecedence
+
+public class MPFloat : Comparable, CustomStringConvertible {
 
     // The value
-    var value: mpfr_t
+    public var value: mpfr_t
     
+    // Temporary value, prevent memory allocation
+    public var tmp: mpfr_t
+
     // Precision (default is 128 bit)
     public let precision: Int32
 
@@ -23,15 +28,18 @@ public class MPFloat : Comparable {
         precision = prec
         value = mpfr_t()
         mpfr_init2(&value, mpfr_prec_t(prec))
+        tmp = mpfr_t()
+        mpfr_init2(&tmp, mpfr_prec_t(prec))
     }
     
     /// Initialize a value with a String
     public init(_ string: String = "0", precision: Int32 = 128) {
         self.precision = precision
+        
         self.value = mpfr_t()
-
-        // Allocate memory
         mpfr_init2(&self.value, mpfr_prec_t(precision))
+        tmp = mpfr_t()
+        mpfr_init2(&tmp, mpfr_prec_t(precision))
         
         // Store value
         mpfr_set_str(&self.value, string, 10, MPFR_RNDN)
@@ -41,18 +49,26 @@ public class MPFloat : Comparable {
     public init(_ dval: Double, precision: Int32 = 128) {
         self.precision = precision
         self.value = mpfr_t()
-        
         mpfr_init2(&self.value, mpfr_prec_t(precision))
+        
         mpfr_set_d(&self.value, dval, MPFR_RNDN)
+        
+        tmp = mpfr_t()
+        mpfr_init2(&tmp, mpfr_prec_t(precision))
     }
 
     /// Free memory
     deinit {
         mpfr_clear(&self.value)
+        mpfr_clear(&self.tmp)
+    }
+    
+    public var description: String {
+        return self.toString()
     }
     
     /// Return a copy of a value
-    /// Use y = x.copy() instead of y = x
+    /// Use y = x.copy() instead of y = x to create a new instance
     func copy() -> MPFloat {
         let newObj = MPFloat(prec: self.precision)
         mpfr_set(&newObj.value, &self.value, MPFR_RNDN)
@@ -64,14 +80,28 @@ public class MPFloat : Comparable {
         mpfr_set_str(&self.value, string, 10, MPFR_RNDN)
     }
     
+    /// Set value to string
+    public static func <- (lhs: MPFloat, _ rhs: String) {
+        mpfr_set_str(&lhs.value, rhs, 10, MPFR_RNDN)
+    }
+
     /// Set value to Double
     public func set(_ dval: Double) {
         mpfr_set_d(&self.value, dval, MPFR_RNDN)
     }
-        
+    
+    /// Set value to Double
+    public static func <- (_ lhs: MPFloat, _ rhs: Double) {
+        mpfr_set_d(&lhs.value, rhs, MPFR_RNDN)
+    }
     /// Set value to value of MPFloat (deep copy)
-    public func set(from other: MPFloat) {
+    public func set(_ other: MPFloat) {
         mpfr_set(&self.value, &other.value, MPFR_RNDN)
+    }
+    
+    /// Set value to value of MPFloat (deep copy)
+    public static func <- (lhs: MPFloat, rhs: MPFloat) {
+        mpfr_set(&lhs.value, &rhs.value, MPFR_RNDN)
     }
     
     /// Convert value to Double
@@ -139,8 +169,9 @@ public class MPFloat : Comparable {
     }
     
     /// Addition (in place)
-    static func += (lhs: inout MPFloat, rhs: MPFloat) {
-        mpfr_add(&lhs.value, &lhs.value, &rhs.value, MPFR_RNDN)
+    public static func += (lhs: inout MPFloat, rhs: MPFloat) {
+        mpfr_add(&lhs.tmp, &lhs.value, &rhs.value, MPFR_RNDN)
+        mpfr_set(&lhs.value, &lhs.tmp, MPFR_RNDN)
     }
     
     //
@@ -156,14 +187,15 @@ public class MPFloat : Comparable {
     
     /// Subtraction (in place)
     public static func -= (lhs: inout MPFloat, rhs: MPFloat) {
-        mpfr_sub(&lhs.value, &lhs.value, &rhs.value, MPFR_RNDN)
+        mpfr_sub(&lhs.tmp, &lhs.value, &rhs.value, MPFR_RNDN)
+        mpfr_set(&lhs.value, &lhs.tmp, MPFR_RNDN)
     }
 
     //
     // Multiplication
     //
     
-    /// Multiplication, return new value (additional memory needed)
+    /// Multiplication: MPFloat with MPFloat
     public static func * (_ lhs: MPFloat, _ rhs: MPFloat) -> MPFloat {
         let result = MPFloat(precision: lhs.precision)
         mpfr_mul(&result.value, &lhs.value, &rhs.value, MPFR_RNDN)
@@ -186,26 +218,53 @@ public class MPFloat : Comparable {
     
     /// Inplace multiplication with MPFloat
     public static func *= (_ lhs: inout MPFloat, _ rhs: MPFloat) {
-        mpfr_mul(&lhs.value, &lhs.value, &rhs.value, MPFR_RNDN)
+        mpfr_mul(&lhs.tmp, &lhs.value, &rhs.value, MPFR_RNDN)
+        mpfr_set(&lhs.value, &lhs.tmp, MPFR_RNDN)
     }
     
     /// Inplace multiplication with Double
     public static func *= (lhs: inout MPFloat, rhs: Double) {
-        mpfr_mul_d(&lhs.value, &lhs.value, rhs, MPFR_RNDN)
+        mpfr_mul_d(&lhs.tmp, &lhs.value, rhs, MPFR_RNDN)
+        mpfr_set(&lhs.value, &lhs.tmp, MPFR_RNDN)
     }
 
-    /// Square, returns new value (additional memory needed)
-    public static func squared(_ lhs: MPFloat) -> MPFloat {
-        let res = MPFloat(prec: lhs.precision)
-        mpfr_pow_ui(&res.value, &lhs.value, 2, MPFR_RNDN)
+    //
+    // Division
+    //
+    
+    /// Divison: MPFloat by MPFloat
+    public static func / (_ lhs: MPFloat, _ rhs: MPFloat) -> MPFloat {
+        let result = MPFloat(precision: lhs.precision)
+        mpfr_div(&result.value, &lhs.value, &rhs.value, MPFR_RNDN)
+        return result
+    }
+    
+    /// Division: Double by MPFloat
+    public static func / (lhs: Double, rhs: MPFloat) -> MPFloat {
+        let res = MPFloat(prec: rhs.precision)
+        mpfr_d_div(&res.value, lhs, &rhs.value, MPFR_RNDN)
         return res
     }
     
-    /// Inplace square
-    public func squared() {
-        mpfr_pow_ui(&self.value, &self.value, 2, MPFR_RNDN)
+    /// Division: MPFloat by Double
+    public static func / (lhs: MPFloat, rhs: Double) -> MPFloat {
+        let res = MPFloat(prec: lhs.precision)
+        mpfr_div_d(&res.value, &lhs.value, rhs, MPFR_RNDN)
+        return res
     }
-
+    
+    /// Division: in-place by MPFloat
+    public static func /= (_ lhs: inout MPFloat, _ rhs: MPFloat) {
+        mpfr_div(&lhs.tmp, &lhs.value, &rhs.value, MPFR_RNDN)
+        mpfr_set(&lhs.value, &lhs.tmp, MPFR_RNDN)
+    }
+    
+    /// Division: in-place by Double
+    public static func /= (lhs: inout MPFloat, rhs: Double) {
+        mpfr_div_d(&lhs.tmp, &lhs.value, rhs, MPFR_RNDN)
+        mpfr_set(&lhs.value, &lhs.tmp, MPFR_RNDN)
+    }
+    
     //
     // Comparision
     //
@@ -229,34 +288,32 @@ public class MPFloat : Comparable {
     public static func > (lhs: MPFloat, rhs: Double) -> Bool {
         return mpfr_cmp_d(&lhs.value, rhs) > 0
     }
-
     
-    /*
-     In der  gezeigten Funktion werden tmp1 und tmp2 als inout-Parameter übergeben. Das ist der "Profi-Trick": Du erstellst diese Hilfsvariablen einmalig vor der Schleife und reichst sie nur herum. Dadurch findet innerhalb der 10.000 Iterationen deines Referenz-Orbits keine einzige Speicherreservierung statt.
-
-    func iterateInPlace(zRe: inout HighPrec, zIm: inout HighPrec, cRe: HighPrec, cIm: HighPrec, tmp1: inout HighPrec, tmp2: inout HighPrec) {
-        // Formel:
-        // newRe = zRe^2 - zIm^2 + cRe
-        // newIm = 2 * zRe * zIm + cIm
-        
-        // 1. tmp1 = zRe^2
-        mpfr_pow_ui(&tmp1.value, &zRe.value, 2, MPFR_RNDN)
-        
-        // 2. tmp2 = zIm^2
-        mpfr_pow_ui(&tmp2.value, &zIm.value, 2, MPFR_RNDN)
-        
-        // 3. Berechne Imaginärteil: zIm = 2 * zRe * zIm + cIm
-        // Wir nutzen mpfr_mul_si für den Faktor 2 (sehr schnell)
-        mpfr_mul(&zIm.value, &zRe.value, &zIm.value, MPFR_RNDN)
-        mpfr_mul_si(&zIm.value, &zIm.value, 2, MPFR_RNDN)
-        zIm += cIm
-        
-        // 4. Berechne Realteil: zRe = tmp1 - tmp2 + cRe
-        mpfr_sub(&zRe.value, &tmp1.value, &tmp2.value, MPFR_RNDN)
-        zRe += cRe
+    //
+    // Mathematic functions
+    //
+    
+    /// Square root, return new value
+    public func sqrt() -> MPFloat {
+        let res = MPFloat(prec: precision)
+        mpfr_sqrt(&res.value, &self.value, MPFR_RNDN)
+        return res
     }
-     
-     */
+    
+    /// Square, return new value
+    public func square() -> MPFloat {
+        let res = MPFloat(prec: precision)
+        mpfr_sqr(&res.value, &self.value, MPFR_RNDN)
+        return res
+    }
+    
+    /// Logarithm, return new value
+    public func log() -> MPFloat {
+        let res = MPFloat(prec: precision)
+        mpfr_log(&res.value, &self.value, MPFR_RNDN)
+        return res
+    }
+
 }
 
 extension Double {
@@ -267,76 +324,3 @@ extension Double {
 }
 
 
-/*
- Referenz Orbit
- 
- func generateReferenceOrbit(cRe: String, cIm: String, maxIter: Int) -> [HighPrecComplex] {
-     let re = HighPrec(cRe)
-     let im = HighPrec(cIm)
-     var zRe = HighPrec("0.0")
-     var zIm = HighPrec("0.0")
-     
-     // Unsere Arbeits-Variablen (Recycling!)
-     var t1 = HighPrec()
-     var t2 = HighPrec()
-     
-     var orbit: [HighPrecComplex] = []
-
-     for _ in 0..<maxIter {
-         // 1. Aktuellen Punkt speichern (muss kopiert werden!)
-         orbit.append(HighPrecComplex(re: zRe.copy(), im: zIm.copy()))
-         
-         // 2. Escape Check
-         if checkEscapeOptimized(zRe: zRe, zIm: zIm, tmp1: &t1, tmp2: &t2) {
-             break
-         }
-         
-         // 3. Nächster Schritt (In-Place)
-         iterateInPlace(zRe: &zRe, zIm: &zIm, cRe: re, cIm: im, tmp1: &t1, tmp2: &t2)
-     }
-     return orbit
- }
- 
- 
- struct ComplexDouble {
-     var re: Double
-     var im: Double
- }
-
- func calculateDeltaPixel(deltaC: ComplexDouble, referenceOrbit: [HighPrecComplex], maxIter: Int) -> Int {
-     var dzRe = deltaC.re
-     var dzIm = deltaC.im
-     
-     for n in 0..<min(referenceOrbit.count - 1, maxIter) {
-         // Hol den Referenzpunkt aus dem 128-Bit Speicher und wandle ihn in Double
-         let ZnRe = referenceOrbit[n].re.toDouble()
-         let ZnIm = referenceOrbit[n].im.toDouble()
-         
-         // Die Perturbations-Formel:
-         // dz_next = 2 * Zn * dz + dz^2 + dc
-         
-         // 1. (2 * Zn * dz)
-         let twoZn_dz_Re = 2 * (ZnRe * dzRe - ZnIm * dzIm)
-         let twoZn_dz_Im = 2 * (ZnRe * dzIm + ZnIm * dzRe)
-         
-         // 2. (dz^2)
-         let dz2Re = dzRe * dzRe - dzIm * dzIm
-         let dz2Im = 2 * dzRe * dzIm
-         
-         // 3. Alles zusammenfügen
-         dzRe = twoZn_dz_Re + dz2Re + deltaC.re
-         dzIm = twoZn_dz_Im + dz2Im + deltaC.im
-         
-         // 4. Escape Check auf Delta-Basis
-         // Achtung: Wir prüfen, ob der absolute Punkt (Z + dz) entkommt!
-         let totalZRe = ZnRe + dzRe
-         let totalZIm = ZnIm + dzIm
-         
-         if (totalZRe * totalZRe + totalZIm * totalZIm) > 4.0 {
-             return n // Punkt ist entkommen
-         }
-     }
-     
-     return maxIter // Punkt ist (wahrscheinlich) in der Menge
- }
- */
